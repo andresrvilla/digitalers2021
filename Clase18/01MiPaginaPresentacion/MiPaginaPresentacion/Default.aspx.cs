3db8f82173ba7569b891bdee8eafe0841f27544d
Andrés Villa <robertoavilla@gmail.com>
2021-07-21 22:09:19 -0300

    Clase 18

---
+++ Clase18/01MiPaginaPresentacion/MiPaginaPresentacion/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiPaginaPresentacion
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private List<string> proyectos;
+
+        public Default()
+        {
+            //proyectos = new List<string>();
+            //proyectos.Add("Pagina de presentación");
+            //proyectos.Add("Blog");
+            //proyectos.Add("Sitio de noticias");
+
+            proyectos = new List<string>()
+            {
+                "Pagina de presentación",
+                "Blog",
+                "Sitio de noticias"
+            };
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lstProyectos.DataSource = proyectos;
+            lstProyectos.DataBind();
+        }
+    }
+}
---
+++ Clase18/01MiPaginaPresentacion/MiPaginaPresentacion/MasInformacion/QuienSoy.aspx.cs
@@ -0,0 +1,58 @@
+﻿using MiPaginaPresentacion.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiPaginaPresentacion.MasInformacion
+{
+    public partial class QuienSoy : System.Web.UI.Page
+    {
+        private List<Comentario> comentarios;
+
+        public QuienSoy()
+        {
+            
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ViewState["comentarios"] == null)
+            {
+                comentarios = new List<Comentario>();
+                ViewState["comentarios"] = comentarios;
+            }
+            else
+            {
+                comentarios = (List<Comentario>)ViewState["comentarios"];
+            }
+
+            lstComentarios.DataSource = comentarios;
+            lstComentarios.DataBind();
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            //El "tipo de dato var"
+            //En realidad es un tipo de dato especifico pero lo toma de la asignacion
+            var comentario = txtComentario.Text;
+
+            Comentario nuevoComentario = new Comentario()
+            {
+                Nombre = nombre,
+                Texto = comentario
+            };
+
+            comentarios.Add(nuevoComentario);
+            ViewState["comentarios"] = comentarios;
+
+            txtComentario.Text = "";
+            txtNombre.Text = "";
+
+            lstComentarios.DataBind();
+        }
+    }
+}
