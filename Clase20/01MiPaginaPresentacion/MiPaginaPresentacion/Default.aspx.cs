b46b1f42807106e39d7b5298ea3ea6f50ea871b4
Andrés Villa <robertoavilla@gmail.com>
2021-08-09 21:58:45 -0300

    Clase 21

---
+++ Clase20/01MiPaginaPresentacion/MiPaginaPresentacion/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MiPaginaPresentacion
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private List<string> proyectos;
+
+        public Default()
+        {
+            //proyectos = new List<string>();
+            //proyectos.Add("Pagina de presentación");
+            //proyectos.Add("Blog");
+            //proyectos.Add("Sitio de noticias");
+
+            proyectos = new List<string>()
+            {
+                "Pagina de presentación",
+                "Blog",
+                "Sitio de noticias"
+            };
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lstProyectos.DataSource = proyectos;
+            lstProyectos.DataBind();
+        }
+
+        protected void btnRedirigirQuienSoy_Click(object sender, EventArgs e)
+        {
+            //Server.Transfer carga la pagina pero no cambia la url
+            //Es decir, carga la pagina en el servidor
+            //Server.Transfer("~/MasInformacion/QuienSoy.aspx");
+
+            //Si quiero que funcione como un link, tengo que hacer esto:
+            Response.Redirect("~/MasInformacion/QuienSoy.aspx");
+        }
+    }
+}
---
+++ Clase20/01MiPaginaPresentacion/MiPaginaPresentacion/MasInformacion/Ocupacion/Default.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace MiPaginaPresentacion.MasInformacion.Ocupacion
+{
+
+
+    public partial class Default
+    {
+
+        /// <summary>
+        /// Control lblContadorViewState.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContadorViewState;
+
+        /// <summary>
+        /// Control btnContarViewState.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnContarViewState;
+
+        /// <summary>
+        /// Control lblContadorSession.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContadorSession;
+
+        /// <summary>
+        /// Control btnContarSession.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnContarSession;
+
+        /// <summary>
+        /// Control lblContadorCookies.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblContadorCookies;
+
+        /// <summary>
+        /// Control btnContadorCookies.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnContadorCookies;
+    }
+}
